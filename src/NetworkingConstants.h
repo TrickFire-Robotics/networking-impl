e58063c28899a838c09a442487fb6fa749eae795
Adam Krpan <adam.krpan@outlook.com>
2017-04-03 17:20:15 -0700

    Slight tweak to definition of the bin sliding constant.

--- src/NetworkingConstants.h
+++ src/NetworkingConstants.h
@@ -5,7 +5,7 @@
 #define CAMERA_PACKET 2
 #define MINER_MOVE_PACKET 3 // -1 to lower, 1 to raise, 0 to stop
 #define MINER_SPIN_PACKET 4 // -1 to empty, 1 to dig, 0 to stop
-#define BIN_SLIDE_PACKET 5 // -1 to slide out of robot, 1 to slide in to robot, 0 to stop
+#define BIN_SLIDE_PACKET 5 // -1 to slide to fill, 1 to slide to dump, 0 to stop
 #define CONVEYOR_PACKET 6 // No parameters
 
 #endif
