2faf8a5d6e049478c4a84bbc64af3f5f3d67f1ae
Adam Krpan <adam.krpan@outlook.com>
2017-05-05 16:12:02 -0700

    Separated stage 1 and 2 of the lift in networking constants.

--- src/NetworkingConstants.h
+++ src/NetworkingConstants.h
@@ -3,9 +3,10 @@
 
 #define DRIVE_PACKET 1
 #define CAMERA_PACKET 2
-#define MINER_MOVE_PACKET 3 // -1 to lower, 1 to raise, 0 to stop
-#define MINER_SPIN_PACKET 4 // -1 to empty, 1 to dig, 0 to stop
-#define BIN_SLIDE_PACKET 5 // -1 to slide to fill, 1 to slide to dump, 0 to stop
-#define CONVEYOR_PACKET 6 // 1 to fill, 0 to stop
+#define MINER_MOVE_S1_PACKET 3 // -1 to lower, 1 to raise, 0 to stop
+#define MINER_MOVE_S2_PACKET 4 // -1 to lower, 1 to raise, 0 to stop
+#define MINER_SPIN_PACKET 5 // -1 to empty, 1 to dig, 0 to stop
+#define BIN_SLIDE_PACKET 6 // -1 to slide to fill, 1 to slide to dump, 0 to stop
+#define CONVEYOR_PACKET 7 // 1 to fill, 0 to stop
 
 #endif
